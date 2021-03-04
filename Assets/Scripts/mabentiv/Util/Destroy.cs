2f294b0e468c1d4cb34d7685ea18c12bb2b5af59
Matteo Bentivegna <matteo.bentivegna@student.hs-rm.de>
2021-03-04 23:40:29 +0100

    nullreference for state machines

---
+++ Assets/Scripts/mabentiv/Util/Destroy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using HSRM.Scripts.mabentiv.Events;
+using UnityEngine.Events;
+using HSRM.Scripts.mabentiv.Commands;
+
+namespace HSRM.Scripts.mabentiv.Utils
+{
+    public class Destroy : BaseCommand
+    {
+        [SerializeField] private BoxCollider go = null;
+
+        protected override void ExecuteCommand()
+        {
+            if (go != null)
+            {
+                Destroy(go);
+            }
+        }
+        private void Update()
+        {
+            Execute();
+        }
+    }
+}
