ed45cc9a1ab8d31a95ee80c65769d5b33c3ba6df
Matteo Bentivegna <matteo.bentivegna@student.hs-rm.de>
2021-03-06 00:17:16 +0100

    destroy skript can destroy multiple objects now + added to fuse handle, teddy and wm cable

--- Assets/Scripts/mabentiv/Util/Destroy.cs
+++ Assets/Scripts/mabentiv/Util/Destroy.cs
@@ -2,18 +2,22 @@
 using HSRM.Scripts.mabentiv.Events;
 using UnityEngine.Events;
 using HSRM.Scripts.mabentiv.Commands;
+using System.Collections.Generic;
 
 namespace HSRM.Scripts.mabentiv.Utils
 {
     public class Destroy : BaseCommand
     {
-        [SerializeField] private Object go = null;
+        [SerializeField] private List<Object> go = null;
 
         protected override void ExecuteCommand()
         {
             if (go != null)
             {
-                Destroy(go);
+                foreach (Object gameOb in go)
+                {
+                    Destroy(gameOb);
+                }
             }
         }
     }
