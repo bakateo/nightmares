0ea50c235ca0ce38dee1f77f633ce0636f14191f
Matteo Bentivegna <matteo.bentivegna@student.hs-rm.de>
2021-03-07 01:42:10 +0100

    light switch script implemented and added to scene

---
+++ Assets/Scripts/mabentiv/Util/LightSwitch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using HSRM.Scripts.mabentiv.Commands;
+using UnityEngine;
+
+namespace HSRM.Scripts.mabentiv.Util
+{
+    public class LightSwitch : BaseCommand
+    {
+        [SerializeField] private List<Light> lights = null;
+        [SerializeField] private bool isOn = false;
+
+        protected override void ExecuteCommand()
+        {
+            isOn = !isOn;
+            foreach(var light in lights)
+            {
+                light.enabled = isOn;
+            }
+        }
+
+        void Start()
+        {
+            foreach (var light in lights)
+            {
+                light.enabled = isOn;
+            }
+        }
+    }
+}
